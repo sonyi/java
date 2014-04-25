7d9403ccf3171beef8a2568075f2c780d3049556
sonyi <wxy15105957760>
2014-04-25 17:47:43 +0800

    w

---
+++ day14/IO流2/src/cn/imcore/test/TestBufferedWriter.java
@@ -0,0 +1,40 @@
+package cn.imcore.test;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TestBufferedWriter {
+
+	
+	public static void main(String[] args) {
+		BufferedWriter bw = null;
+		try {
+			bw = new BufferedWriter(new FileWriter("data/temp1.txt"));
+			bw.write("qqqqqqqqq");
+			bw.write(13);//  \r »Ø³µ
+			bw.write(10);//  \n »»ÐÐ
+			bw.write("qqqqqqqqq");
+			bw.newLine();
+			bw.write("qqqqqqqqq");
+			bw.newLine();
+			bw.write("qqqqqqqqq");
+			bw.newLine();
+			bw.write("qqqqqqqqq");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(bw != null) {
+				try {
+					bw.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+}
---
+++ day14/IO流2/src/cn/imcore/test/TestPrintWriter.java
@@ -0,0 +1,18 @@
+package cn.imcore.test;
+
+import java.io.PrintStream;
+
+public class TestPrintWriter {
+
+	
+	public static void main(String[] args) {
+//		System.out.println("aaa");
+		PrintStream ps = System.out;//打印到控制台
+		ps.println("aaa");
+		ps.println("aaa");
+		ps.println("aaa");
+		ps.println("aaa");
+
+	}
+
+}
